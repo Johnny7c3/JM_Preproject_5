3617dd1b60e476116d7a8065cbd6c276ffb81284
Johnny7c3 <johnny7c3@gmail.com>
2020-03-10 12:43:40 +0300

    Correction_6

---
+++ src/main/java/servlet/AddUserServlet.java
@@ -0,0 +1,36 @@
+package servlet;
+
+import service.UserService;
+import service.UserServiceImpl;
+import model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/add")
+public class AddUserServlet extends HttpServlet {
+    private UserService serv;
+
+    @Override
+    public void init() throws ServletException {
+        serv = UserServiceImpl.getInstance();
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        String email = req.getParameter("email");
+        String name = req.getParameter("name");
+        String surname = req.getParameter("surname");
+        String password = req.getParameter("password");
+        String birthday = req.getParameter("birthday");
+        String role = req.getParameter("role");
+        User newUser = new User(email, name, surname, password, birthday, role);
+        serv.addUser(newUser);
+        res.sendRedirect("/admin");
+    }
+}
